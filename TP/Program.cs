bed2a828ecf7a0745b296a0da4eccdd96e611984
Chanple <chanple@outlook.com>
2018-06-19 15:02:23 +0800

    ~

---
+++ TP/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+using static TP.SafeNativeMethods;
+
+namespace TP
+{
+    static class Program
+    {
+        static void Main()
+        {
+            var accent = new AccentPolicy()
+            {
+                AccentState = 3,
+                AccentFlags = 0x100,
+            };
+
+            var accentPtr = Marshal.AllocHGlobal(Marshal.SizeOf(accent));
+            Marshal.StructureToPtr(accent, accentPtr, false);
+
+            var data = new WindowCompositionAttributeData()
+            {
+                Attribute = 19,
+                SizeOfData = Marshal.SizeOf(accent),
+                Data = accentPtr
+            };
+
+            IntPtr hwnd = FindWindowW("Shell_TrayWnd", null);
+            while (true)
+            {
+                SetWindowCompositionAttribute(hwnd, ref data);
+                Thread.Sleep(125);
+            }
+
+            //Marshal.FreeHGlobal(accentPtr);
+        }
+    }
+}
---
+++ TP/SafeNativeMethods.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP
+{
+    [SuppressUnmanagedCodeSecurity]
+    static class SafeNativeMethods
+    {
+        [DllImport("user32.dll")]
+        internal static extern int SetWindowCompositionAttribute(IntPtr hwnd, ref WindowCompositionAttributeData data);
+
+        [StructLayout(LayoutKind.Sequential)]
+        internal struct AccentPolicy
+        {
+            public int AccentState;
+            public int AccentFlags;
+            public int GradientColor;
+            public int AnimationId;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        internal struct WindowCompositionAttributeData
+        {
+            public int Attribute;
+            public IntPtr Data;
+            public int SizeOfData;
+        }
+
+        [DllImport("user32.dll", EntryPoint = "FindWindowW")]
+        public static extern IntPtr FindWindowW([In()] [MarshalAs(UnmanagedType.LPWStr)] string lpClassName, [In()] [MarshalAs(UnmanagedType.LPWStr)] string lpWindowName);
+
+    }
+}
